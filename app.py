b41bb73d8c99620170a7d00f9efbb8e1f154d4e5
lsngmin <lsngmin@naver.com>
2024-04-01 12:25:48 +0900

    port change

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def getSeries(df,option):
     return series
  
 if __name__ == "__main__":
-    app.run(debug = True, host='0.0.0.0', port=5002, passthrough_errors=True)
+    app.run(debug = True, host='0.0.0.0', port=5000, passthrough_errors=True)
